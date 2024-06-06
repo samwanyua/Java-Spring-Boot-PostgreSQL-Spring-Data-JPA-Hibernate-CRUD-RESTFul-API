df2c5d9a9e0cfa95d434fd81e14ae1ba1f67f82e
samwanyua <samexample8@gmail.com>
2024-06-06 12:17:46 +0300

    Initial commit

---
+++ Postgres Crud/Postgres-Crud/src/test/java/com/wanyua/Postgres/Crud/PostgresCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanyua.Postgres.Crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
